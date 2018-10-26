4ebf7b8d9f5345ef56d9560f8d74467c959e56fc
Adrian Pothuaud <adrianpothuaud@gmail.com>
2018-10-27 01:24:57 +0200

    Impl

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ mongodb.MongoClient.connect(MONGODB_URI, function (err, database) {
   console.log("Database connection ready");
 
   // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
+  var server = app.listen(process.env.PORT || 5000, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
